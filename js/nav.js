e4867c4796b5689182b90cf95a8b9964c238f023
Cristiana Man <man.cristiana1@gmail.com>
2021-04-01 19:45:20 +0200

    Split navigation and typing logic

--- js/nav.js
+++ js/nav.js
@@ -5,8 +5,3 @@ hamburger.onclick = function () {
   hamburger.classList.toggle("open");
   menu.classList.toggle("open");
 };
-
-const typed = new Typed("#typed", {
-  strings: ["Motivating <br />Empowering <br /> Driving Change"],
-  typeSpeed: 50
-});
