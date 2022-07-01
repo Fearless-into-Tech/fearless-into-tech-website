c322d32f9d63ba7f59b02ce390fa966debbb5008
Cristiana Man <man.cristiana1@gmail.com>
2022-07-01 19:36:20 +0200

    Update website content

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,31 @@
-const hamburger = document.querySelector(".hamburger");
+const header = document.querySelector("header");
+const menuButton = document.querySelector("nav button");
 const menu = document.querySelector(".menu");
+const menuAnchors = menu.querySelectorAll("a");
 
-hamburger.onclick = function () {
-  hamburger.classList.toggle("open");
+menuButton.onclick = function () {
+  menuButton.classList.toggle("open");
   menu.classList.toggle("open");
 };
+
+menuAnchors.forEach(function (anchor) {
+  anchor.onclick = function () {
+    menuButton.classList.remove("open");
+    menu.classList.remove("open");
+  };
+});
+
+const toggleMenuButtonVisibilityOnScroll = function () {
+  if (window.scrollY >= 160) {
+    header.classList.add("with-nav-button");
+    menuButton.classList.add("show");
+  } else {
+    header.classList.remove("with-nav-button");
+    menuButton.classList.remove("show");
+    menuButton.classList.remove("open");
+    menu.classList.remove("open");
+
+  }
+};
+
+window.addEventListener("scroll", toggleMenuButtonVisibilityOnScroll);
